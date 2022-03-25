6f5f6389aef663709e0c944ee377cef6941b7253
miru-porto <portocarreromiranda@gmail.com>
2022-03-25 18:47:59 -0300

    agregando categorias

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { FlatList, View } from 'react-native'
 import { styles } from './styles'
+import { CATEGORIES } from '../../constants/categories'
+import CategoryGrid from '../../components/molecules/category-grid/index'
 
-const Home = ({navigation}) => {
+const Home = ({ navigation  }) => {
+    const handleSelectCategory = (category) => {
+        navigation.navigate('Category', { id: category.id, name: category.name })
+    }
+    const renderItem = ({ item }) => <CategoryGrid item={item} onSelected={handleSelectCategory} />
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Home Screen</Text>
-            <Button title="Go to Category" onPress={() => {
-                navigation.navigate('Category')
-                
-                }} />
+            <FlatList 
+                data={CATEGORIES}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+            />
         </View>
     )
 }
