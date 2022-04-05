647ee0b29d86e17ba7b5ab2353127de3d53d2c08
miru-porto <portocarreromiranda@gmail.com>
2022-04-04 22:34:38 -0300

    agregando login

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import categoryReducer from "./reducers/category.reducer";
 import productReducer from "./reducers/product.reducer";
+import cartReducer from "./reducers/cart.reducer";
+import authReducer from "./reducers/auth.reducer";
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     categories: categoryReducer,
     products: productReducer,
+    cart: cartReducer,
+    auth: authReducer
 });
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(thunk));
